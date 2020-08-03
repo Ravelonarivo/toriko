0a36b034feb1386349e8ae235f5508a2ebe6f1ad
ravelonarivo <irinaravelonarivo@gmail.com>
2020-08-03 22:59:57 +0000

    create results content

---
+++ pages/content/results.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+const Results = () => {
+	return (
+		<>
+			<Head>
+				<title>Results</title>
+			</Head>
+			<h1>Results</h1>
+		</>
+	);
+};
+
+export default Results;
