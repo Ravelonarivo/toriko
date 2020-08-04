8867e85482a4ed337850e4016b54ba3ece7ae636
ravelonarivo <irinaravelonarivo@gmail.com>
2020-08-04 13:04:28 +0000

    create utils module css

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,5 @@
-import styles from './layout.module.css'
+import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
 
 const Layout = ({ children }) => {
 	return <div>{ children }</div>
