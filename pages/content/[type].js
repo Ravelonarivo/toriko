088c823bda2b6081b2aee61403287757b3528cb7
ravelonarivo <irinaravelonarivo@gmail.com>
2020-08-06 00:35:01 +0000

    get locations from choice

---
+++ pages/content/[type].js
@@ -0,0 +1,41 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router'
+import fetch from 'node-fetch';
+
+const Result = ({ locations }) => {
+	console.log(locations);
+	const router = useRouter();
+	const type = router.query.type;
+
+	return (
+		<div>
+			<Head>
+				<title>Type</title>
+			</Head>
+			<h1>{ type }</h1>
+		</div>
+	);
+};
+
+export const getStaticPaths = async () => {
+	const response = await fetch('http://localhost:3004/type');
+	const types = await response.json();
+
+	const paths = types.map(type => ({
+		params: { type: type.name }
+	}));
+
+	return { 
+		paths, 
+		fallback: false 
+	};
+}; 
+
+export const getStaticProps = ({ params }) => {
+	return fetch(`http://localhost:3004/location?type=${ params.type }`)
+		.then(response => response.json())
+		.then(locations => ({ props: { locations }}))
+		.catch(error => console.log(error));
+};
+
+export default Result;
