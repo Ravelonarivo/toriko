8f94ca99cebb5fcb25565a17d988793a811f8984
ravelonarivo <irinaravelonarivo@gmail.com>
2020-08-13 13:55:08 +0000

    add showlocation img

--- components/Chart/Chart.js
+++ components/Chart/Chart.js
@@ -123,15 +123,30 @@ class Chart extends Component {
 				    	coords 
 				    		? 	<div>
 				    				<Marker icon={ geolocIcon } position={ userLocation }/>
-				    				<Control position="topleft">
-	  									<button title="Afficher votre localisation" onClick={ getCurrentLocation }>
-	  										P
-	  									</button>
+				    				<Control position="topleft"> 									
+	  									<img className="showlocation" src="/chart/showlocation.png" onClick={ getCurrentLocation } title="Afficher votre localisation"/>	  									
 	  								</Control>
 	  							</div>
 				    		: '' 
 				    	}
 	  			</Map>
+	  			<style jsx>
+	  				{`
+	  					.showlocation {
+	  						width: 26px;
+	  						background: #fff;
+	  						border: solid 1px #999;
+    						border-radius: 5px;
+    						box-shadow: 0 0 4px -1px #333;
+    						padding: 5px;
+	  					}
+
+	  					.showlocation:hover {
+	  						background: #f4f4f4;
+	  						cursor: pointer;
+	  					}
+	  				`}
+	  			</style>
 			</div>
 		);
 	}
