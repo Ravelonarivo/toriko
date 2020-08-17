94acaae9d0788d6d08fbadb9bb4fcda9737b689d
ravelonarivo <irinaravelonarivo@gmail.com>
2020-08-17 00:49:03 +0000

    edit comment

--- pages/result/[type].js
+++ pages/result/[type].js
@@ -55,8 +55,8 @@ const Result = ({ locationsProp }) => {
 			<Head>
 				<title>{ locationsType }</title>
 				<link 
-					rel="stylesheet"
 					// require by leaflet 
+					rel="stylesheet"
 					href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" 
 					integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" 
 					crossOrigin=""
