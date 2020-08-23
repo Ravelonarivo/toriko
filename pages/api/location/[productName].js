abaf886ccc623e3cc4655430d2cabc4b65220f96
ravelonarivo <irinaravelonarivo@gmail.com>
2020-08-23 13:55:52 +0000

    update db query

--- pages/api/location/[productName].js
+++ pages/api/location/[productName].js
@@ -4,9 +4,9 @@ export default ({ query: { productName } }, res) => {
 	return new Promise((resolve, reject) => {
 		db('location')
 			.join('product', 'location.id', '=', 'product.location_id')
+			.join('location_type', 'location.type_id', '=', 'location_type.id')
 			.where('product.name', productName)
 			.select('location.id', 'location.name', 'location.latitude as lat', 'location.longitude as long', 'location_type.name as type')
-			.join('location_type', 'location.type_id', '=', 'location_type.id')
 		.then(locations => {
 			if (locations.length > 0) {
 					res.status(200).json(locations);
