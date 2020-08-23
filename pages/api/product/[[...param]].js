8d6f067a75526f67742d2610e10f3bc53c8c953e
ravelonarivo <irinaravelonarivo@gmail.com>
2020-08-23 13:16:19 +0000

    remove unused code

--- pages/api/product/[[...param]].js
+++ pages/api/product/[[...param]].js
@@ -8,6 +8,10 @@ export default ({ query: { param }}, res) => {
 				.join('location', 'product.location_id', '=', 'location.id')
 				.where('location.type_id', locationsTypeId)
 				.distinct('product.name')
+				/*
+					.distinct('product_type.name')
+					.join('product_type', 'product.type_id', '=', 'product_type.id')
+				*/
 			.then(products => {
 				if (products.length > 0) {
 					res.status(200).json(products);
@@ -34,39 +38,4 @@ export default ({ query: { param }}, res) => {
 			})
 		}
 	});
-
-	/*return new Promise((resolve, reject) => {
-		if (locationsType === 'afficher-tout') {
-			db('location')
-				.join('location_type', 'type_id', '=', 'location_type.id')
-				.where('location.name', 'like', `%${searchFieldInput}%`)
-				.select('location.name', 'location.latitude as lat', 'location.longitude as long', 'location_type.name as type')
-			.then(filteredLocations => {
-				if (filteredLocations.length > 0) {
-					res.status(200).json(filteredLocations);
-					resolve();
-				}
-			})
-			.catch(error => {
-				res.status(400).json('error getting filteredLocations');
-				resolve();
-			})	
-		} else {
-			db('location')
-				.join('location_type', 'type_id', '=', 'location_type.id')
-				.where('location.name', 'like', `%${searchFieldInput}%`)
-				.andWhere('location_type.name', locationsType)
-				.select('location.name', 'location.latitude as lat', 'location.longitude as long', 'location_type.name as type')
-			.then(filteredLocations => {
-				if (filteredLocations.length > 0) {
-					res.status(200).json(filteredLocations);
-					resolve();
-				}
-			})
-			.catch(error => {
-				res.status(400).json('error getting filteredLocations');
-				resolve();
-			})
-		}
-	});	*/
 };
