3ec5644d8b09dc228676f02fa3cde970b329aada
ravelonarivo <irinaravelonarivo@gmail.com>
2020-09-06 00:42:12 +0000

    install nextjs-progressbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NextNprogress from 'nextjs-progressbar';
 
 const App = ({ Component, pageProps }) => {
-	return <Component { ...pageProps } />
+	return (
+		<div>
+			<NextNprogress />
+			<Component { ...pageProps } />
+		</div>
+	);
 };
 
 export default App;
