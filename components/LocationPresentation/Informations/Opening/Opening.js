ad788e29cb120f92b07102fc60df3d5e15083644
ravelonarivo <irinaravelonarivo@gmail.com>
2020-10-02 03:07:46 +0000

    sort Opening component's days

--- components/LocationPresentation/Informations/Opening/Opening.js
+++ components/LocationPresentation/Informations/Opening/Opening.js
@@ -3,6 +3,8 @@ const Opening = ({ openings }) => {
 		return time.slice(0, 5).replace(':','h');
 	}
 
+	const days = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche'];
+	
 	return (
 		<div className="db dib-ns w-50-l w-100-m fr">
 			<div className="flex justify-center">
@@ -10,18 +12,22 @@ const Opening = ({ openings }) => {
 			</div>
 			<div className="bt b--light-silver ml3 pv4 ph4">
 				{
-					openings.map((opening, index) => (
-						<dl className="f6 lh-title mv2 w-100" key={index}>
-							<dt className="dib w-20">{opening.day}</dt>
-							<dd className="dib w-70 tr">
-								{
-									opening.open && opening.close
-										? formatTime(opening.open) + '-' + formatTime(opening.close)
-										: ' fermé'
-								}
-							</dd>
-						</dl>
-					))
+					days.map(day => {
+						return openings.map((opening, index) => (
+							day === opening.day
+								? 	<dl className="f6 lh-title mv2 w-100" key={index}>
+										<dt className="dib w-20">{opening.day}</dt>
+										<dd className="dib w-70 tr">
+											{
+												opening.open && opening.close
+													? formatTime(opening.open) + '-' + formatTime(opening.close)
+													: ' fermé'
+											}
+										</dd>
+									</dl>
+								:   ''						
+						))
+					})
 				}
 			</div>
 		</div>
