53af8d3292483481120d34134bb9737e4407f1c0
ravelonarivo <irinaravelonarivo@gmail.com>
2020-10-04 03:36:42 +0000

    grow logo when hover or click it

--- components/LocationPresentation/Logo/Logo.js
+++ components/LocationPresentation/Logo/Logo.js
@@ -1,7 +1,7 @@
 const Logo = ({ logo }) => {
 	return (
 		<div className="flex justify-center">
-			<div className="pa1 absolute bg-white w4 h4 ba b--light-silver br3" style={{ zIndex: '1', marginTop: '-74px' }}>
+			<div className="grow pointer pa1 absolute bg-white w4 h4 ba b--light-silver br3" style={{ zIndex: '1', marginTop: '-74px' }}>
 			  	<img
 				    src={ logo }
 				    className="br3 h-100 w-100" alt="logo"
