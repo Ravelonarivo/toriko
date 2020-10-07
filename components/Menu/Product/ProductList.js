bc2d1c94f2ff455d4c47df60c4921a870db36a60
ravelonarivo <irinaravelonarivo@gmail.com>
2020-10-07 21:10:47 +0000

    put productType in plural

--- components/Menu/Product/ProductList.js
+++ components/Menu/Product/ProductList.js
@@ -1,12 +1,14 @@
 import Product from './Product';
 
+import { putInPlural } from '../../../lib/functions';
+
 const ProductList = ({ products, productTypes }) => {
 	return (
 		<div>
 			{
 				productTypes.map((productType, index) =>  (
 					<div key={ index }>
-						<h2 className={ index === 0 ? 'tc' : 'tc mt5' }>{ productType.name }s</h2>
+						<h2 className={ index === 0 ? 'tc' : 'tc mt5' }>{ putInPlural(productType.name) }</h2>
 						{
 							products.map((product, index) =>
 								product.type === productType.name
--- components/ProductTypesNav/ProductType/ProductType.js
+++ components/ProductTypesNav/ProductType/ProductType.js
@@ -1,6 +1,8 @@
+import { putInPlural } from '../../../lib/functions';
+
 const ProductType = ({ productType }) => {
 	return (
-		<li className="dib fn ba br4 ph4 mr3 b--moon-gray pointer h2 light-silver hover-black">{ productType.name }s</li>
+		<li className="dib fn ba br4 ph4 mr3 b--moon-gray pointer h2 light-silver hover-black">{ putInPlural(productType.name) }</li>
 	);
 };
 
