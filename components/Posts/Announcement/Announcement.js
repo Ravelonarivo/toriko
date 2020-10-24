28ae8b424a6723600d3e69a3d808bd634c2b74ca
ravelonarivo <irinaravelonarivo@gmail.com>
2020-10-24 00:37:59 +0000

    edit Announcement component style

--- components/Posts/Announcement/Announcement.js
+++ components/Posts/Announcement/Announcement.js
@@ -2,10 +2,10 @@ import { formatText } from '../../../lib/functions';
 
 const Announcement = ({ announcement }) => {
 	return (
-		<div className="w-100 flex mb3 br1 h3 pa1 shadow-4 pointer grow">
+		<div className="w-100 flex mb2 br1 h3 pa1 shadow-4 pointer shadow-hover">
 			{
 				announcement.image 
-					? 	<div className="w3 mr1">
+					? 	<div className="w3 mr1 grow">
 							<img className="w-100 h-100 br1" src={ announcement.image} />
 						</div>
 					: 	''
